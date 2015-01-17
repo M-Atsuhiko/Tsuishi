a5243703dd3e6a759ff2b934114ef2c0bd2095b5
M-Atsuhiko <is0042px@ed.ritsumei.ac.jp>
2015-01-17 15:26:05 +0900

    1/17

--- Tsuishi_TREE_simulation.R
+++ Tsuishi_TREE_simulation.R
@@ -11,14 +11,14 @@ source(paste(Dir,"calc_contraction.R",sep=""))
 source(paste(Dir,"Stem_diam.R",sep=""))
 source(paste(Dir,"calc_Conductance_amount.R",sep=""))
 
-WITH_K <- FALSE
-WITH_Ca <- TRUE
+WITH_K <- TRUE
+WITH_Ca <- FALSE
 RAND_SEED <- 1
-DELTA_T <- 25
-Function_ratio <- 75
-Conductance_ratio <- 5
+DELTA_T <- 5
+Function_ratio <- 90
+Conductance_ratio <- 0
 Morphology_ratio <- 100 - (Function_ratio + Conductance_ratio*(WITH_K || WITH_Ca))
-extra_prefix <- paste("Rerative_liner_",Function_ratio,"_",Conductance_ratio,sep="")
+extra_prefix <- paste("Tsuishi_liner_",Function_ratio,"_",Conductance_ratio,sep="")
 
 if(WITH_K*WITH_Ca){
   name <- "k_ca"
